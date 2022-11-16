c88fc015c87b049334986fbdc63f6e1f7792730d
Markus Bilz <mail@markusbilz.com>
2022-11-16 13:30:15 +0000

    Simplified `setup` and `test_environment`

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Code for thesis at Karlsruhe Institute of Technology',
-    author='Markus Bilz',
-    license='BSD-3',
+    version="0.1.0",
+    description="Code for thesis at Karlsruhe Institute of Technology",
+    author="Markus Bilz",
+    license="BSD-3",
 )
