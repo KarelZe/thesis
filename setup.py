01765f1bbc83b334c688496d4e2152c3f51e247e
Markus Bilz <mail@markusbilz.com>
2022-11-17 16:15:07 +0000

    Formatted files to fullfil pre-commit tests 🐙 - added `typing` support - added new pre-commit hooks - resolved code issues
    Relates to Model training and optimization 🎯 #7

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Setup information for the project.
+
+Code initally generated for my master's thesis.
+"""
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Code for thesis at Karlsruhe Institute of Technology',
-    author='Markus Bilz',
-    license='BSD-3',
+    version="0.1.0",
+    description="Code for thesis at Karlsruhe Institute of Technology",
+    author="Markus Bilz",
+    author_email="markus.bilz@student.kit.edu",
+    license="BSD-3",
 )
--- test_environment.py
+++ test_environment.py
@@ -1,25 +1,38 @@
+"""
+Tests if the environment is suited for development.
+
+Raises:
+        ValueError: Unrecognized python interpreter.
+        TypeError: Python version does not match required version.
+"""
 import sys
 
 REQUIRED_PYTHON = "python3"
 
 
-def main():
+def main() -> None:
+    """
+    Test, if the development environment is suited.
+
+    Raises:
+        ValueError: python interpreter can not be found.
+        TypeError: python version can not be found.
+    """
     system_major = sys.version_info.major
     if REQUIRED_PYTHON == "python":
         required_major = 2
     elif REQUIRED_PYTHON == "python3":
         required_major = 3
     else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
+        raise ValueError(f"Unrecognized python interpreter: {REQUIRED_PYTHON}")
 
     if system_major != required_major:
         raise TypeError(
-            "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
+            f"This project requires Python {required_major}. Found: {sys.version}"
+        )
     else:
         print(">>> Development environment passes all tests!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
