d53d204a82534a27f0e3154513a0ed67163663c1
Markus Bilz <github@markusbilz.com>
2022-11-29 14:18:10 +0100

    Add tests for `TabDataSet` ⛑️
    Relates to #7

---
+++ tests/test_dataset.py
@@ -0,0 +1,123 @@
+"""
+Perform automated tests.
+
+Includes tests for data sets with categorical and without
+categorical data.
+"""
+
+import unittest
+
+import numpy as np
+import pandas as pd
+import torch
+
+from otc.data.dataloader import TabDataLoader
+from otc.data.dataset import TabDataset
+
+
+class TestDataLoader(unittest.TestCase):
+    """
+    Perform automated tests.
+
+    Args:
+        unittest (_type_): testcase
+    """
+
+    def test_len(self) -> None:
+        """
+        Test, if length returned by data set is correct.
+
+        Lenth is simply the number of rows of input data frame.
+        """
+        length = 10
+        x = pd.DataFrame(np.arange(30).reshape(length, 3))
+        y = pd.Series(np.arange(length))
+
+        training_data = TabDataset(x=x, y=y)
+        assert len(training_data) == length
+
+    def test_invalid_len(self) -> None:
+        """
+        Test, if an error is raised if length of x and y do not match.
+
+        If input data is inconsistent, it should not be further processed.
+        """
+        length = 10
+        x = pd.DataFrame(np.arange(30).reshape(length, 3))
+        # make y one element longer
+        y = pd.Series(np.arange(length + 1))
+
+        self.assertRaises(AssertionError, lambda: TabDataset(x=x, y=y))
+
+    def test_invalid_unique_count(self) -> None:
+        """
+        Test, if an error is raised if length of 'cat_features' and 'cat_unique_counts'\
+        do not match.
+
+        Models like TabTransformer require to know the number of unique values for each
+        categorical feature, as it used in the embedding layer.
+        """
+        length = 10
+        x = pd.DataFrame(np.arange(30).reshape(length, 3), columns=["a", "b", "c"])
+        y = pd.Series(np.arange(length))
+
+        self.assertRaises(
+            AssertionError,
+            lambda: TabDataset(
+                x=x, y=y, cat_features=["a", "b"], cat_unique_counts=tuple([20])
+            ),
+        )
+
+    def test_with_cat_features(self) -> None:
+        """
+        Test, if data loader can be created with categorical features.
+
+        If the data set contains categorical features, the data loader
+        should return a tensor with the categorical features.
+        """
+        length = 3
+        x = pd.DataFrame(
+            np.arange(length * 3).reshape(length, 3), columns=["a", "b", "c"]
+        )
+        y = pd.Series(np.arange(length))
+
+        # column a is categorical
+        training_data = TabDataset(
+            x=x, y=y, cat_features=["a"], cat_unique_counts=tuple([100])
+        )
+        train_loader = TabDataLoader(
+            training_data._x_cat,
+            training_data._x_cont,
+            training_data._y,
+            batch_size=2,
+            shuffle=False,
+        )
+        cat_features, _, _ = next(iter(train_loader))
+
+        self.assertTrue(torch.tensor([[0], [3]]).equal(cat_features))  # type: ignore
+
+    def test_no_cat_features(self) -> None:
+        """
+        Test, if data loader can be created without categorical features.
+
+        If data set doesn't contain categorical features, the data loader
+        should return None.
+        """
+        length = 3
+        x = pd.DataFrame(
+            np.arange(length * 3).reshape(length, 3), columns=["a", "b", "c"]
+        )
+        y = pd.Series(np.arange(length))
+
+        # no categorical features
+        training_data = TabDataset(x=x, y=y, cat_features=None, cat_unique_counts=None)
+        train_loader = TabDataLoader(
+            training_data._x_cat,
+            training_data._x_cont,
+            training_data._y,
+            batch_size=2,
+            shuffle=False,
+        )
+        cat_features, _, _ = next(iter(train_loader))
+
+        self.assertIsNone(cat_features)
