a57020019a4d2b27b3d5e7b3c704c7fdf4f610f4
Markus Bilz <mail@markusbilz.com>
2023-03-13 19:19:29 +0100

    Generate results for classical classifier + effective spread👸 (#200)
    Adresses #8.

--- src/otc/metrics/metrics.py
+++ src/otc/metrics/metrics.py
@@ -7,22 +7,40 @@ import numpy as np
 import numpy.typing as npt
 from sklearn.utils import check_consistent_length
 
+from typing import Literal
+
 
 def effective_spread(
-    y_pred: npt.NDArray, trade_price: npt.NDArray, fundamental_value: npt.NDArray
+    y_pred: npt.NDArray,
+    trade_price: npt.NDArray,
+    fundamental_value: npt.NDArray,
+    mode: Literal["nominal", "relative"] = "nominal",
 ) -> np.float64:
     """
-    Calculate the effective spread given by:
+    Depending on mode, calculate the nominal effective spread given by:
     $$
     S_{i,t} = 2 (P_{i,t} - V_{i,t}) D_{i,t}
+    $$
+
+    Calculate the relative effective spread given by:
+    $$
+    {PS}_{i,t} = S_{i,t} / V_{i,t}.
     $$
 
     Args:
         y_pred (npt.NDArray): indicator if the trade is a buy or sell
         trade_price (npt.NDArray): trade price
         fundamental_value (npt.NDArray): fundamental value e. g., bid-ask midpoint.
+        mode (Literal["nominal", "relative"], optional): "nominal" or "relative". Defaults to "nominal".
     Returns:
         float: average effective spread
     """
     check_consistent_length(y_pred, trade_price, fundamental_value)
-    return np.mean(2 * (trade_price - fundamental_value) * y_pred)
+    s = 2 * (trade_price - fundamental_value) * y_pred
+    if mode == "nominal":
+        return np.nanmean(s)
+    else:
+        # nan when div by zero https://stackoverflow.com/a/54364060/5755604
+        ps = np.empty(y_pred.shape)
+        np.divide(s, fundamental_value, out=ps, where=fundamental_value != 0)
+        return np.nanmean(ps)
