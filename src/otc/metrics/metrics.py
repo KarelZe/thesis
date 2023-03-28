792bc02e8a5ffeb54d15c549559cdda6fa19b042
Markus Bilz <github@markusbilz.com>
2023-03-28 15:22:29 +0200

    Update metrics.py🐍

--- src/otc/metrics/metrics.py
+++ src/otc/metrics/metrics.py
@@ -3,20 +3,24 @@ Sklearn implementation of effective spread.
 
 See: https://hagstromer.org/2020/11/23/overestimated-effective-spreads/ for explanation.
 """
+from __future__ import annotations
+
+from typing import Literal
+
 import numpy as np
 import numpy.typing as npt
 from sklearn.utils import check_consistent_length
-
-from typing import Literal
 
 
 def effective_spread(
     y_pred: npt.NDArray,
     trade_price: npt.NDArray,
     fundamental_value: npt.NDArray,
-    mode: Literal["nominal", "relative"] = "nominal",
-) -> np.float64:
+    mode: Literal["nominal", "relative", "none"] = "nominal",
+) -> np.float64 | npt.NDArray:
     """
+    Calculate the effective spread.
+
     Depending on mode, calculate the nominal effective spread given by:
     $$
     S_{i,t} = 2 (P_{i,t} - V_{i,t}) D_{i,t}
@@ -27,11 +31,15 @@ def effective_spread(
     {PS}_{i,t} = S_{i,t} / V_{i,t}.
     $$
 
+    If mode is "none", return the effective spread without averaging.
+
     Args:
         y_pred (npt.NDArray): indicator if the trade is a buy or sell
         trade_price (npt.NDArray): trade price
-        fundamental_value (npt.NDArray): fundamental value e. g., bid-ask midpoint.
-        mode (Literal["nominal", "relative"], optional): "nominal" or "relative". Defaults to "nominal".
+        fundamental_value (npt.NDArray): fundamental value e. g., bid-ask
+        midpoint.
+        mode (Literal["nominal", "relative", "none"], optional): "nominal" or
+        "relative" or "none". Defaults to "nominal".
     Returns:
         float: average effective spread
     """
@@ -39,8 +47,9 @@ def effective_spread(
     s = 2 * (trade_price - fundamental_value) * y_pred
     if mode == "nominal":
         return np.nanmean(s)
-    else:
+    elif mode == "relative":
         # nan when div by zero https://stackoverflow.com/a/54364060/5755604
         ps = np.empty(y_pred.shape)
         np.divide(s, fundamental_value, out=ps, where=fundamental_value != 0)
         return np.nanmean(ps)
+    return s
