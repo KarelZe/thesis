8a9ac6a27079354851cc3c17765594c818a9ff46
Markus Bilz <github@markusbilz.com>
2023-05-03 17:46:25 +0200

    fix typo 🐞

--- src/otc/optim/scheduler.py
+++ src/otc/optim/scheduler.py
@@ -1,20 +1,21 @@
+from typing import List
+
 import numpy as np
 from torch import optim
 
-from typing import List
 
 class CosineWarmupScheduler(optim.lr_scheduler._LRScheduler):
-    def __init__(self, optimizer:optim.optimizer, warmup:int, max_iters:int):
+    def __init__(self, optimizer: optim.Optimizer, warmup: int, max_iters: int):
         self.warmup = warmup
         self.max_num_iters = max_iters
         super().__init__(optimizer)
 
-    def get_lr(self)->List[float]:
+    def get_lr(self) -> List[float]:
         lr_factor = self.get_lr_factor(epoch=self.last_epoch)
         return [base_lr * lr_factor for base_lr in self.base_lrs]
 
-    def get_lr_factor(self, epoch:int) -> float:
-       
+    def get_lr_factor(self, epoch: int) -> float:
+
         lr_factor = 0.5 * (1 + np.cos(np.pi * epoch / self.max_num_iters))
         if epoch <= self.warmup:
             lr_factor *= epoch * 1.0 / self.warmup
