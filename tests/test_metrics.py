13a1de443a42f20b068e1c63935f5905046c3af9
Markus Bilz <github@markusbilz.com>
2023-10-31 07:52:32 +0100

    fix pytest

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -18,7 +18,7 @@ class TestMetrics:
 
         Value may not be NaN.
         """
-        rng = np.random.default_rng(seed=7)
+        rng = np.random.RandomState(seed=7)
         y_pred = rng.choice([-1, 1], size=(10))
         trade_price = rng.random(10) * 100
         fundamental_value = rng.random(10) * 100
